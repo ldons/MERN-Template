f043c269e6f5712dcc056b2cee02c91ff6a1f2a2
Liridon Selmani <liridon.slm@gmail.com>
2024-12-07 16:19:57 +0100

    footer link

--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -1,21 +1,23 @@
 const Footer = () => {
   return (
     <footer className="bg-white border-t">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="py-4">
-            <div className="flex items-center justify-between">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="py-4">
+          <div className="flex items-center justify-between">
+            <a href="https://github.com/ldons/MERN-Template" target="_blank">
               <div className="flex items-center gap-2">
                 <span className="text-gray-600">Made with</span>
                 <img alt="MERN Template" src="logo.svg" className="size-6" />
                 <span className="text-gray-600">MERN Template</span>
               </div>
-              <p className="text-base text-gray-500">
-                © {new Date().getFullYear()} MERN Template. All rights reserved.
-              </p>
-            </div>
+            </a>
+            <p className="text-base text-gray-500">
+              © {new Date().getFullYear()} MERN Template. All rights reserved.
+            </p>
           </div>
         </div>
-      </footer>
+      </div>
+    </footer>
   );
 };
 
